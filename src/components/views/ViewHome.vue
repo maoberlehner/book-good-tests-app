bae2f0794b71e9997fdbdcc4fbd35a0eb988f2d0
Markus Oberlehner <markus@oberlehner.net>
2023-09-17 18:45:42 +0200

    Remove shopping list feature

--- src/components/views/ViewHome.vue
+++ src/components/views/ViewHome.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import BaseHeadline from '../BaseHeadline.vue';
-import ShoppingList from '../ShoppingList.vue';
 
 const isLoggedIn = Boolean(localStorage.getItem('token'));
 </script>
@@ -8,7 +7,7 @@ const isLoggedIn = Boolean(localStorage.getItem('token'));
 <template>
   <div v-if="isLoggedIn" class="space-y-4">
     <BaseHeadline level="1">Shopping list</BaseHeadline>
-    <ShoppingList />
+    <p>TODO</p>
   </div>
   <div v-else>
     <RouterLink to="/login">Log in</RouterLink>
