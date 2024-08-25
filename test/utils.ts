7609e3cd94972619761e1f5b6dc15de3420727dc
Markus Oberlehner <markus@oberlehner.net>
2024-08-25 11:26:02 +0200

    Component test mocking and userEvent setup

--- test/utils.ts
+++ test/utils.ts
@@ -1,13 +1,25 @@
 import userEvent from '@testing-library/user-event';
 import { makeMockEndpoint } from './mock-endpoint';
 import { mockServer } from './mock-server';
+import { render, type RenderOptions } from '@testing-library/vue';
 import type { Prepare } from './driver';
+import type { Component } from 'vue';
 
-export { render, screen } from '@testing-library/vue';
+export { screen } from '@testing-library/vue';
 export { expect, it } from 'vitest';
-export { mockServer, userEvent };
+export { mockServer };
 
 export const mockEndpoint = makeMockEndpoint({ mockServer });
 
-export const prepare: Prepare = (precondition) =>
-  precondition({ mockEndpoint });
+export const prepare: Prepare = async (precondition) =>
+  precondition({ mockEndpoint, localStorage });
+
+export const setup = (
+  component: Component,
+  { renderOptions }: { renderOptions?: RenderOptions } = {},
+) => {
+  return {
+    user: userEvent.setup(),
+    ...render(component, renderOptions),
+  };
+};
